e525533f66fcac5aa9b8d3103964d4667fe3920b
Marc-Eli Faldas <marc.eli.faldas@gmail.com>
2018-05-07 16:24:05 -0400

    Rough Draft 1.5: Changed most styles and added to list

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,24 +1,28 @@
 @extends('layouts.master')
 
 @section('title')
-   About Marc
+    About Marc
 @endsection
 
 @section('content')
     <h1>ABOUT</h1>
 
     <p>
-        Conjugation Filipino Verbs is a little bit hard.  That is why we have this useful dictionary!  Find verbs, add them to your list, and start practicing!
+        Conjugation Filipino Verbs is a little bit hard. That is why we have this useful dictionary! Find verbs, add them to your list, and start practicing!
     </p>
 
     <p>
-        Ang conjugation na mga Pilipino pandiwa ay mahirap.  Kaya nga mayroon diksyunaryo kami para kayo!  Hanapin mo mga pandiwa, lagayin nyo sa listahan ninyo, atsaka praktisin nyo!
+        Ang conjugation na mga Pilipino pandiwa ay mahirap. Kaya nga mayroon diksyunaryo kami para kayo! Hanapin mo mga pandiwa, lagayin nyo sa listahan ninyo, atsaka praktisin nyo!
     </p>
 
     <p>
         コンジュゲーションのフィリピンのバーブは難しいです。それは、私たちはこの辞書があります。バーブを拾ったりリストに付けたり練習します！
     </p>
 
+    <p>
 
+        La conjugación de los verbos filipinos es difícil. Por eso, tenemos este diccionario para usted. Buscan verbos, agregalos a su lista y practica!
+
+    </P>
 
 @endsection
